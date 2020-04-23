7c967629eafaca34edca4135b198f6ecd0648882
Kumar Priyansh <bobbyskhs@gmail.com>
2020-04-24 00:56:31 +0530

    Removed debug lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
-import { TranslateService } from '@ngx-translate/core';
-import { AppConfig } from '../environments/environment';
+// import { TranslateService } from '@ngx-translate/core';
+// import { AppConfig } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +11,18 @@ import { AppConfig } from '../environments/environment';
 export class AppComponent {
   constructor(
     public electronService: ElectronService,
-    private translate: TranslateService
+    // private translate: TranslateService
   ) {
-    translate.setDefaultLang('en');
-    console.log('AppConfig', AppConfig);
+    // translate.setDefaultLang('en');
+    // console.log('AppConfig', AppConfig);
 
-    if (electronService.isElectron) {
-      console.log(process.env);
-      console.log('Mode electron');
-      console.log('Electron ipcRenderer', electronService.ipcRenderer);
-      console.log('NodeJS childProcess', electronService.childProcess);
-    } else {
-      console.log('Mode web');
-    }
+    // if (electronService.isElectron) {
+    //   console.log(process.env);
+    //   console.log('Mode electron');
+    //   console.log('Electron ipcRenderer', electronService.ipcRenderer);
+    //   console.log('NodeJS childProcess', electronService.childProcess);
+    // } else {
+    //   console.log('Mode web');
+    // }
   }
 }
